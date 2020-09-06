1024ef3c17aee749066d31ceb49f936c0313c316
jonatas <jonatas.rodolfo@info.ufrn.br>
2020-09-06 15:49:20 -0300

    Iniciando o desenvolvimento da aplicação Mobile, páginas e nagevação.

---
+++ mobile/src/pages/TeacherList/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        borderStartColor: '#F0F0F0',
+    },
+});
+
+export default styles;
